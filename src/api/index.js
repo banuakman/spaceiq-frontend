18c8fd6d8bb78df7d47d61da60fb62ca0f449d19
banuakman <banu@asterya.com>
2021-06-25 13:33:54 -0400

    add dependencies - create placeholder pages for components & pages

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,15 @@
 import ky from "ky";
 
-const bookingUrl = "http://localhost:8080/api/booking/";
-const employeeUrl = "http://localhost:8080/api/employee/";
+const bookingUrl = "http://localhost:8080/api/bookings/";
+const employeeUrl = "http://localhost:8080/api/employees/";
+const employeeBookingsUrl = "http://localhost:8080/api/employeeBookings/";
 
 const API = {
   // BOOKING
-  getAllBookings() {
-    return ky.get(bookingUrl).json();
-  },
   getBookingById(id) {
     return ky.get(bookingUrl + id).json();
   },
+
   createBooking(booking) {
     return ky.post(bookingUrl, { json: booking }).json();
   },
@@ -21,6 +20,12 @@ const API = {
   // EMPLOYEE
   updateEmployee(request) {
     return ky.put(employeeUrl, { json: request }).json();
+  },
+
+  // BOOKING BY EMPLOYEE
+
+  getBookingByEmployeeId(employeeId) {
+    return ky.get(employeeBookingsUrl + employeeId).json();
   },
 };
 
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import { Heading } from "@chakra-ui/react";
+
+function Header() {
+  return (
+    <header className="container mx-auto">
+      <Heading as="h1">Private Header</Heading>
+    </header>
+  );
+}
+export default Header;
