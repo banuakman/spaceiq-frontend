ce4d18d1498aa3caee822cae0ab7a881f3e9944e
Priya Garg <gupta.ria100@gmail.com>
2021-06-28 03:19:27 -0500

    adding select date

--- src/api/index.js
+++ src/api/index.js
@@ -3,16 +3,21 @@ import ky from "ky";
 const bookingUrl = "http://localhost:8042/edge/bookings/";
 const employeeUrl = "http://localhost:8042/edge/employee/";
 
-const deskUrl = "http://localhost:8080/api/desks/";
+const deskUrl = "http://localhost:8042/edge/availableDesks";
 
 const api = {
   // BOOKING
   getAllBookings() {
     return ky.get(bookingUrl).json();
   },
-  getAllDesks() {
-    return ky.get(deskUrl).json();
+  getAvailableDeskByDate(date) {
+    return ky.get(`${deskUrl}?date=${date}`).json();
+  
   },
+  // getAvailableDeskByDate(date) {
+  //   return ky.get(`${deskUrl}?date=2021-9-30`).json();
+  
+  // },
 
   getBookingsById(id) {
     return ky.get(bookingUrl + id).json();
